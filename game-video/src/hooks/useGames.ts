443cdc9b62ff441f2ee697b019eed8ca6a0c5d3a
Mehmet Ayan <mayn3507@gmail.com>
2024-02-25 20:55:15 +0100

    Show loading skeletons

--- game-video/src/hooks/useGames.ts
+++ game-video/src/hooks/useGames.ts
@@ -24,20 +24,28 @@ interface FetchGamesResponse {
 const useGames = () => {
     const [games , setGames ] = useState<Game[]>([]);
     const [error , setError] = useState('');
+    const  [isLoading , setIsLoading] = useState(true) ;
     
 
 useEffect(() => {
     const controller = new AbortController();
+    setIsLoading(true);
+
     apiClient
     .get<FetchGamesResponse>('/games', { signal:controller.signal })  //adds a timeout to the request
-    .then(res => setGames(res.data.results))
+    .then(res => {
+        setGames(res.data.results);
+        setIsLoading(false);
+    } )
     .catch(err => {
         if (err instanceof CanceledError) return;
-    setError(err.message) });
+    setError(err.message) 
+    setIsLoading(false);
+});
 
     return  ()=> controller.abort();   //cleanup function that will be called when this component is unmounted (removed from
     
 },[]);
- return { games , error }; 
+ return { games , error, isLoading }; 
 }
 export default useGames
