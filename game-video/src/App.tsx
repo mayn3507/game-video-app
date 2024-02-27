5a7cf1ff9422e606678cc519dd94e789ceb645a5
Mehmet Ayan <mayn3507@gmail.com>
2024-02-27 18:06:46 +0100

    Building Sort Selector

--- game-video/src/App.tsx
+++ game-video/src/App.tsx
@@ -1,10 +1,11 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import { useState } from "react";
 import './App.css';
 import GameGrid from './components/GameGrid';
 import GenreList from "./components/GenreList";
 import NavBar from "./components/NavBar";
 import PlatformSelector from "./components/PlatformSelector";
+import SortSelector from "./components/SortSelector";
 import { Platform } from "./hooks/useGames";
 import { Genre } from "./hooks/useGenres";
 
@@ -37,8 +38,11 @@ function App() {
         <GenreList selectedGenre={gameQuery.genre} onSelectGenre={(genre) => setGameQuery({...gameQuery, genre})} />
         </GridItem>
         </Show>
-     <GridItem area ="main" >
-      <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform)=>setGameQuery({...gameQuery, platform})} />
+        <GridItem area ="main" >
+     <HStack spacing= {5} paddingLeft={2} marginBottom={5}>
+         <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform)=>setGameQuery({...gameQuery, platform})} />
+          <SortSelector/>
+     </HStack>
        <GameGrid gameQuery={gameQuery} />
         </GridItem>
         
