c5148b4d08a26b82dc78b9e29c88e233f21c51a2
Mehmet Ayan <mayn3507@gmail.com>
2024-02-28 01:01:03 +0100

    Clean up the Genres

--- game-video/src/components/GenreList.tsx
+++ game-video/src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Spinner } from '@chakra-ui/react';
+import { Button, HStack, Heading, Image, List, ListItem, Spinner } from '@chakra-ui/react';
 import useGenres, { Genre } from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 
@@ -14,21 +14,25 @@ const GenreList = ({selectedGenre, onSelectGenre} : Props) => {
       if (isLoading) return <Spinner/>;  // Loading...
 
   return (
+    <>
+    <Heading fontSize='3xl' marginBottom={3}>Genres</Heading>
     <List>
         {data.map((genre)=>(
         <ListItem key={genre.id} paddingY={5}>
        <HStack>
         <Image 
-        boxSize='32px' 
+        boxSize='40px' 
         borderRadius = {8} 
+        objectFit='cover'
         src = { getCroppedImageUrl(genre.image_background)}/>
-        <Button fontWeight={genre.id === selectedGenre?.id ? 'bold': 'normal'} onClick={()=> onSelectGenre(genre )}
+        <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold': 'normal'} onClick={()=> onSelectGenre(genre )}
         fontSize='lg' variant='link'>{genre.name}
         </Button>
         </HStack>
         </ListItem>
         ))}
     </List>
+    </>
   )
   };
 
